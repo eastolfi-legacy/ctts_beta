36ca9cbf7feade359f3bbf622545a6662589c6e2
eduardo <eastolfi91@gmail.com>
2013-05-30 01:27:02 +0200

    Get

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Controllers */
-function DiscografiaCtrl($scope) {
+function DiscografiaCtrl($scope, $http) {
     $scope.collapsedItems = {'Maqueta' : true, 'Disco_1' : true, 'Live' : true};
     $scope.open = function(parent, item) {
         $scope.shouldBeOpen = true;
@@ -21,17 +21,28 @@ function DiscografiaCtrl($scope) {
         $scope.collapsedItems[itemId] = !$scope.collapsedItems[itemId];
     }
 
-    var content = '<h1>ola ke ase</h1>';
-
+    $scope.panesContent = ['', ''];
     $scope.panes = [
-        { title:"Imágenes", content:content, active: true },
-        { title:"Videos", content:"Dynamic content 2" }
+        { title:"Imágenes", content:$scope.panesContent[0], active: true },
+        { title:"Videos", content:$scope.panesContent[1] }
     ];
     var maqueta = ['Track 1', 'Track 2', 'Track 3'];
     var disco1 = ['Cancion 1', 'Cancion 2'];
     var live = ['Intro', 'Cancion 1', 'Cancion 2', 'Ending'];
     var discos = {'Maqueta':maqueta, 'Disco_1':disco1, 'Live':live};
     $scope.discos = discos;
+
+    var urlDev = 'http://localhost:3000/discos';
+    var urlProd = 'http://ctts_server.eastolfi.c9.io/discos';
+
+    $http({
+            method: 'GET',
+            url: urlProd,
+            contentType: 'jsonp',
+            headers: {'Access-Control-Allow-Origin' : '*'}
+    }).success(function(date) {
+        alert(JSON.parse(data[0]));
+    });
 }
 
 
